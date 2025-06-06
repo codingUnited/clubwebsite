f20cf9e380c9e9730064cdf1b6a83e88dddbc68d
ashinaola <ashinaola@pop-os.localdomain>
2025-06-06 03:33:20 -0700

    Added first unit test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# test to check if the page rendered
+class TestCases(TestCase):
+    def test_call_to_render(self):
+        response = self.client.get('../home.html')
+        self.assertEqual(response.status_code, 200)
