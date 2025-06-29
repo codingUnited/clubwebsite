e5ecf898b553e3a9f4599044b48b8bc7125ed160
Jose de Lima <delimajm@gmail.com>
2025-06-29 18:12:42 -0500

    override default 404 to display custom page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, about
+from .views import home, about, errorPage
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
 ]
+
+handler404 = errorPage
