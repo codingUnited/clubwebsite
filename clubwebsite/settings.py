55ffa8a2bcf3b1b735bb5c1dedc8c511a6105338
Jose de Lima <delimajm@gmail.com>
2025-08-03 21:55:00 -0500

    add loging

--- clubwebsite/settings.py
+++ clubwebsite/settings.py
@@ -179,3 +179,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if not DEBUG:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
