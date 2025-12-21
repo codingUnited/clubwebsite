af836e609057bd47f10eeb451701795332ac79e3
Squee247 <jschmidt901318@gmail.com>
2025-12-21 15:58:27 -0600

    Changes to About Us page. Added Meetings Page

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import home, about, errorPage, contact
+from .views import home, about, errorPage, contact, session
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
 
-    path('contact/', contact, name='contact')
+    path('contact/', contact, name='contact'),
+    path('session/', session, name='session'),
+
 ]
 
 handler404 = errorPage
