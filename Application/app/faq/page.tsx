6c6ab0a173314dcb3f7b6030d4c3bde5069ae6b6
Squee247 <jschmidt901318@gmail.com>
2026-02-18 21:33:25 -0600

    feat: enhance FAQ and Home page with accordion layout and new hero image

--- Application/app/faq/page.tsx
+++ Application/app/faq/page.tsx
@@ -1,17 +1,26 @@
 import faqs from "@/data/faqs/testSessions.json";
+import { Accordion, Span } from "@chakra-ui/react/";
+
 export default function Faqs() {
   return (
     <>
-      <div>
-        {faqs.map((index) => (
-          <>
-            <div>{index.title}</div>
-            <div>{index.response}</div>
-            <div>{index.date.split("T")[0]}</div>
-            <div>{index.category}</div>
-          </>
+      <Accordion.Root collapsible defaultValue={["b"]} spaceY="4">
+        {faqs.map((faq) => (
+          <Accordion.Item key={faq.title} value={faq.title}>
+            <Accordion.ItemTrigger>
+              <Span flex="1" color="black" fontWeight="bold">
+                 {faq.title}
+              </Span>
+              <Accordion.ItemIndicator />
+            </Accordion.ItemTrigger>
+            <Accordion.ItemContent>
+              <Accordion.ItemBody color="gray.600">
+                 {faq.response}
+              </Accordion.ItemBody>
+            </Accordion.ItemContent>
+          </Accordion.Item>
         ))}
-      </div>
+      </Accordion.Root>
     </>
   );
 }
